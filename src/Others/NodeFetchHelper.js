b41becca2205559e0bb09d60232942f0fac3a89c
Fayaz Bin Salam <p32929@users.noreply.github.com>
2020-12-29 12:58:01 +0600

    Update NodeFetchHelper.js

--- src/Others/NodeFetchHelper.js
+++ src/Others/NodeFetchHelper.js
@@ -1,3 +1,7 @@
+// Created by p32929
+// URL: https://gist.github.com/p32929/6143fafc629dcc1e7352d8ca268d11d1
+// Feel free to give it a star
+
 //
 const GET = "GET";
 const POST = "POST";
@@ -90,5 +94,34 @@ export class NodeFetchHelper {
                 console.log(status);
                 callback(status, jsonData)
             });
+    }
+
+    static upload = (url, params, fileKeyString, fileObj, callback) => {
+        if (params) {
+            params = new URLSearchParams(params);
+            url = url + "?" + params
+        }
+
+        var formData = new FormData()
+        formData.append('type', 'file')
+        formData.append(fileKeyString, fileObj)
+
+        fetch(url, {
+            method: POST,
+            headers: {
+                Accept: 'application/json',
+            },
+            body: formData
+        }).then(res => Promise.all([res.status, res.json()]))
+            .then(([status, jsonData]) => {
+                console.log(jsonData);
+                console.log(status);
+                callback(status, jsonData)
+            })
+            .catch((e) => {
+                callback(500, {
+                    error: e
+                })
+            })
     }
 }
